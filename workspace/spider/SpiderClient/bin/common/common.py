dfcdfe2193ce13243beccbd4defb46387d41c0ca
zldeng <dengzhilong.mioji.com>
2016-12-23 09:13:40 +0800

    add some tool to write file for hotel

--- workspace/spider/SpiderClient/bin/common/common.py
+++ workspace/spider/SpiderClient/bin/common/common.py
@@ -9,6 +9,7 @@
 import jsonlib
 import time
 from util import http_client
+from logger import logger
 
 proxy_client = http_client.HttpClientPool("10.136.8.94:8086")
 #proxy_client2 = http_client.HttpClientPool("10.136.11.134:8087")
@@ -26,8 +27,8 @@ def set_proxy_client(client):
     global proxy_client2
     proxy_client2 = client
 
-frame_ip = '10.10.48.27|10.10.38.160|10.10.29.204|10.10.106.179|10.10.228.4|10.10.218.199|10.10.246.77|10.10.153.6|10.10.95.70|10.10.84.225|10.10.100.30|10.10.111.212|10.10.99.125|'
-verify_cn_ip = '10.10.156.56|10.10.184.17|10.10.184.214|10.10.170.233|10.10.176.6'
+frame_ip = '10.10.48.27|10.10.38.160|10.10.29.204|10.10.106.179|10.10.228.4|10.10.218.199|10.10.246.77|10.10.153.6|10.10.95.70|10.10.84.225|10.10.100.30|10.10.111.212|10.10.99.125|10.10.156.116|'
+verify_cn_ip = '10.10.156.116|10.10.156.56|10.10.184.17|10.10.184.214|10.10.170.233|10.10.176.6'
 
 def get_proxy(source = None, allow_ports = [], forbid_ports = [], allow_regions = [], forbid_regions = [], user = 'realtime', passwd = 'realtime', proxy_info = {}):
     special_ip = frame_ip + '|' + verify_cn_ip
@@ -96,7 +97,51 @@ def update_proxy(source_name, proxy, start_time, error_code):
 
     return None
 
+def getStrMd5(srcStr):
+    import hashlib
+    myMd5 = hashlib.md5()
+    myMd5.update(srcStr)
+    strMd5 = myMd5.hexdigest()
+
+    return strMd5
+
+def writerTupleDataToFile(rooms_list,out_file):
+    try:
+        import json
+        for cand_room in rooms_list:
+            out_file.write(json.dumps(cand_room) + '\n')
+    except Exception,e:
+        logger.error('writer data to tmp file fail ' + str(e))
+        import sys
+        sys.exit(1)
+        pass
+
+def getTupleDataFromFile(file_name):
+    '''
+    将酒店例行抓取的数据存储到临时文件中,避免内存的大量使用
+    抓取完成后从文件中读取所有数据然后插入数据库
+    '''
+    data_list = []
+    import json
+    try:
+        in_file = file(file_name)
+        for line in in_file:
+            line = line.strip()
+
+            if '' == line:
+                continue
+
+            cand_data = json.loads(line)
+
+            data_list.append(cand_data)
+    except Exception,e:
+        logger.error('get data from file fail.error = ' + str(e) +'\n')
+
+    import os
+    os.system('rm ' + file_name)
+    return data_list
 
 if __name__ == '__main__':
-    p = get_proxy(forbid_regions=['CN'])
-    print p
+    #p = get_proxy(forbid_regions=['CN'])
+    cand_str = 'test str'
+    print getStrMd5(cand_str)
