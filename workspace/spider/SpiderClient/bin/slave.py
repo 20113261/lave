6f6f18460e65a86634a70ec2587c049044248f39
dujun <dujun@mioji.com>
2017-05-25 19:12:22 +0800

    调整酒店例行任务并发量

--- workspace/spider/SpiderClient/bin/slave.py
+++ workspace/spider/SpiderClient/bin/slave.py
@@ -400,9 +400,9 @@ if __name__ == "__main__":
 
         if 'ListHotel' in task_type:
             import workload
-            workload.TASK_COUNT = 150
-            greents_num = 30
-            mioji.common.pool.pool.set_size(1024)
+            workload.TASK_COUNT = 400
+            #greents_num = 30
+            mioji.common.pool.pool.set_size(4096)
             mioji.common.spider.need_write_file = False
         else:
             mioji.common.pool.pool.set_size(4096)
