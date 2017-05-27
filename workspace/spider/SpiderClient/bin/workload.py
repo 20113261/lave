50416674f87f0af919e4562bc6cfaff2ed7543da
changjing <changjing@mioji.com>
2017-05-27 11:23:06 +0800

    删除一条巨多的log

--- workspace/spider/SpiderClient/bin/workload.py
+++ workspace/spider/SpiderClient/bin/workload.py
@@ -87,7 +87,7 @@ class ControllerWorkload(WorkloadStorable):
             result = result.strip('\0').strip()
             self.newtasks = eval(result)
             logger.info(
-                'from master get taskcount is : {0} / {1} \n tasks: {2}'.format(len(self.newtasks), need_task, result))
+                'from master get taskcount is : {0} / {1}'.format(len(self.newtasks), need_task))
         except Exception, e:
             logger.info('GET TASKS ERROR: ' + str(e))
             return False
