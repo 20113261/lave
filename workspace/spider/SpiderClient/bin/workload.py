cba3c68cb9747ff6f7f3f6a861c0526beef6420c
dujun <dujun@mioji.com>
2017-07-17 17:23:29 +0800

    修改验证数据redis时长

--- workspace/spider/SpiderClient/bin/workload.py
+++ workspace/spider/SpiderClient/bin/workload.py
@@ -118,7 +118,7 @@ class ControllerWorkload(WorkloadStorable):
             rds = redis.Redis(host=task.redis_host, port=task.redis_port, db=int(
                 task.redis_db), password=task.redis_passwd)
             result = {"err_code": Error, "data": proxy}
-            rds.setex(task.redis_key, json.dumps(result), 600)
+            rds.setex(task.redis_key, json.dumps(result), 1800)
         except Exception, e:
             logger.info('writer redis fail. result:{0}'.format(proxy))
             logger.info('writer redis fail.' + str(task.redis_key) + '\t'
