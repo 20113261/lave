b072a9309dfb84a5fc514a9fd2ac2eab9950d7e1
peifei <peifei@mioji.com>
2017-09-25 11:28:23 +0800

    添加日志信息

--- workspace/spider/SpiderClient/bin/slave.py
+++ workspace/spider/SpiderClient/bin/slave.py
@@ -136,7 +136,8 @@ def work(task):
             logger.info('creating instance cost\t [%s ms] \t [%s]' % (
                 str(etime - stime), task.source))
         except Exception, e:
-
+            logger.error("[新框架 爬虫抛出异常: task_data:%s  error:%s]",
+                             task.task_data, str(e))
             error_info = str(traceback.format_exc().split('\n'))
             error = WORK_ERROR
             workload.complete_workload(task, error, proxy_or_ticket)
